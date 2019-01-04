02c3dee13ded093202c32a0fa925419bc89eab45
RodrigoSoares <1140420@isep.ipp.pt>
2019-01-04 14:41:27 +0000

    Created config file

---
+++ config.ts
@@ -0,0 +1,7 @@
+export class Config{
+    // SiC_GC connection
+    public static "urlSiC_GC" = "https://sicarqsi.azurewebsites.net";
+
+    // SiC_E connection
+    public static "urlSiC_E" = "https://sic-e.herokuapp.com";
+};
