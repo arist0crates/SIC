3d27d960f65fc7d7ee41889f2f60ed4c71715306
Pedro Tedim <1091234@isep.ipp.pt>
2019-01-04 01:35:44 +0000

    app is now able to store its currentuser role to be able to use appropriate routes for its role

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,31 +2,35 @@ import { Router } from '@angular/router';
 import * as firebase from 'firebase';
 import { Injectable, SystemJsNgModuleLoader } from '@angular/core';
 import { User } from './user';
+import { forEach } from '@angular/router/src/utils/collection';
+import { stringify } from '@angular/core/src/util';
 
 
 @Injectable()
 export class AuthService {
   token: string;
+  private currentuserrole : string;
 
   constructor(private router: Router) { }
   uid: string;
   signupUser(email: string, password: string, nome: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password).then((returnedUser) => {
       this.writeUserData(returnedUser.uid, email, true, false, false, false, nome);
-     // this.log(returnedUser, "registered");
+      // this.log(returnedUser, "registered");
+      this.signinUser(email, password);
+      this.router.navigate(['/']);
     })
       .catch(
         error => console.log(error)
       )
-
-
-
-
   }
-  getCurrentUserUid() {
 
+
+
+  getCurrentUserUid() {
     return firebase.auth().currentUser.uid;
   }
+
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
@@ -37,21 +41,22 @@ export class AuthService {
             .then(
               (token: string) => this.token = token
             )
-            const d = new Date().toTimeString();
-            this.log(firebase.auth().currentUser.uid, "logged in",d)
+          const d = new Date().toTimeString();
+          this.log(firebase.auth().currentUser.uid, "logged in", d)
         }
 
       )
       .catch(
         error => console.log(error)
       );
+    this.getCurrentUserRoles();
   }
 
   logout() {
     firebase.auth().signOut();
     this.token = null;
     const d = new Date().toString();
-    this.log(firebase.auth().currentUser.uid,"logged out",d);
+    this.log(firebase.auth().currentUser.uid, "logged out", d);
   }
 
   getToken() {
@@ -65,11 +70,13 @@ export class AuthService {
   isAuthenticated() {
     return this.token != null;
   }
+  
   writeUserData(userId, email, customer, contentManager, orderManager, clericalWorker, nome) {
 
     firebase.database().ref('users/' + userId).set({
       nome: nome,
       email: email
+
 
     });
 
@@ -89,23 +96,74 @@ export class AuthService {
     });
     return null;
   }
-  log(uid , whatUsed: string, d: string) {
- 
-  
+  log(uid, whatUsed: string, d: string) {
+
+
     firebase.database().ref('log/' + "abcd").set({
       user: uid,
-      date : d,
+      date: d,
       whatIsUsed: whatUsed
 
     });
   }
-  deleteUser(){
+  deleteUser() {
     firebase.auth().currentUser.delete();
-      // User deleted.
-   var ref = firebase.database().ref(
-    "users/".concat(firebase.auth().currentUser.uid, "/")
- );
- ref.remove();
+    // User deleted.
+    var ref = firebase.database().ref(
+      "users/".concat(firebase.auth().currentUser.uid, "/")
+    );
+    ref.remove();
     this.logout();
   }
+
+  getCurrentUserRoles() {
+    //Get the user data
+    var myUserId = firebase.auth().currentUser.uid;
+    var userrolesRef = firebase.database().ref('users/' + myUserId).orderByChild('roles');
+
+    let getroles = function () {
+      return new Promise((resolve, reject) => {
+        userrolesRef.on('value', function (snapshot) {
+          var childData = JSON.parse(JSON.stringify(snapshot.val().roles));
+          resolve(childData)
+        });
+      });
+    };
+
+    getroles().then(childData => {
+      if (childData["clericalWorker"] == true) {
+        this.isClericalWorker();
+      } else if (childData["contentManager"] == true) {
+        this.isContentManager();
+      } else if (childData["orderManager"] == true) {
+        this.isOrderManager();
+      } else{
+        this.isCustomer();
+      }
+
+      console.log(this.currentuserrole);
+
+    })
+  }
+
+  isContentManager() {
+    this.currentuserrole = "contentManager"
+  }
+
+  isClericalWorker() {
+    this.currentuserrole = "clericalWorker"
+  }
+
+  isOrderManager() {
+    this.currentuserrole = "orderManager"
+  }
+
+  isCustomer() {
+    this.currentuserrole = "customer"
+  }
+
+  getRole(){
+    return this.currentuserrole;
+  }
+
 }
