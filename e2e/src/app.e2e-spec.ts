2a5b4dcea01a736ecf9ef795829b3419a62d010c
= <1160900@isep.ipp.pt>
2019-01-06 17:45:54 +0000

    e2e tests work

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,12 +1,34 @@
 import { AppPage } from './app.po';
 import { element, by } from 'protractor';
 import { browser } from 'protractor';
+import { NgSelectMultipleOption, SelectMultipleControlValueAccessor } from '@angular/forms/src/directives';
 describe('workspace-project App', () => {
   browser.get('http://localhost:4200');
+  it('should be home view', () => {
+  
+    browser.ignoreSynchronization = true;
+    browser.waitForAngular();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/');
+  });
+
 
   it('should change to Register view', () => {
     var register = element(by.id('register'));
+    console.log("abcd");
+    if(register==null){
+      console.log("abcd");
+    }
+    register.click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/signup');
+  });
+  it('should change to Login view', () => {
+    var register = element(by.id('login'));
+    console.log("abcd");
+    if(register==null){
+      console.log("abcd");
+    }
     register.click();
     expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/signin');
   });
+
 });
